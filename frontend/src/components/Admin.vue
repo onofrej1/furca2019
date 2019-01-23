d808bdb31692b73c5cf276c41ff61728a65abaed
Erik Onofrej <erik.onofrej@centrum.sk>
2019-01-24 00:25:29 +0100

    commit

--- frontend/src/components/Admin.vue
+++ frontend/src/components/Admin.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-toolbar dark color="primary">
+    <v-toolbar dark color="primary" app>
       <v-toolbar-side-icon></v-toolbar-side-icon>
 
       <v-toolbar-title class="white--text">O5 Bežecký klub Furča</v-toolbar-title>
@@ -11,7 +11,7 @@
       </v-btn>
     </v-toolbar>
 
-    <v-navigation-drawer clipped  permanent>
+    <v-navigation-drawer clipped  permanent absolute style='top: 65px'>
       <v-list dense class="pt-0">
         <template v-for="(item) in items">
           <v-list-tile :key="item.title">
@@ -38,7 +38,7 @@
         <router-view></router-view>
       </v-container>
     </v-content>
-    <v-footer app></v-footer>
+    <v-footer style="border:1px solid lightgray" app></v-footer>
   </div>
 </template>
 
